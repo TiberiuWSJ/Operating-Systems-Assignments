c8e4f005d9f8f201ef23e0eac659985304edd93e
rodanciuc <tibi.rodanciuc@gmail.com>
2023-05-04 02:21:29 +0300

    second

--- a2/a2.c
+++ a2/a2.c
@@ -4,17 +4,48 @@
 #include <sys/wait.h>
 #include "a2_helper.h"
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct {
+    int thread_no;
+ } TH_STRUCT;
+
+sem_t sem1, sem2;
+
+
+void *threadFn(void* param){
+    TH_STRUCT *s = (TH_STRUCT*)param;
+    info(BEGIN, 2, s->thread_no);
+
+
+    info(END, 2, s->thread_no);
+
+    return NULL;
+}
 
 int main()
 {
     init();
-
+    TH_STRUCT params[5];
+    pthread_t tid[5];
+    
     info(BEGIN, 1, 0);
 
     int pid = fork();
     if (pid == 0)
     {
         info(BEGIN, 2, 0);
+    
+        for(int i = 0; i < 5; i++){
+            params[i].thread_no = i+1;
+            pthread_create(&tid[i], NULL, threadFn, &params[i]);
+        }
+
+        for(int i = 0; i < 5; i++){
+            pthread_join(tid[i], NULL);
+        }
+
         int pid3 = fork();
         if (pid3 == 0)
         {
