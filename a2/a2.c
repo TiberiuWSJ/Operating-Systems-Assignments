da83e67d5f875d0a6875e70880a9f539e64fcde4
rodanciuc <tibi.rodanciuc@gmail.com>
2023-05-04 19:21:45 +0300

    started work on threads barrier

--- a2/a2.c
+++ a2/a2.c
@@ -7,34 +7,49 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-typedef struct {
+typedef struct
+{
     int thread_no;
-    sem_t* sem1;
-    sem_t* sem2;
- } TH_STRUCT;
+    sem_t *sem1;
+    sem_t *sem2;
+} TH_STRUCT;
 
+void *threadFn(void *param)
+{
+    TH_STRUCT *s = (TH_STRUCT *)param;
 
-
-void *threadFn(void* param){
-    TH_STRUCT *s = (TH_STRUCT*)param;
-
-    if(s->thread_no == 2){
+    if (s->thread_no == 2)
+    {
         sem_wait(s->sem1);
         info(BEGIN, 2, s->thread_no);
         info(END, 2, s->thread_no);
         sem_post(s->sem2);
-
-    }else if(s->thread_no == 3){
+    }
+    else if (s->thread_no == 3)
+    {
         info(BEGIN, 2, s->thread_no);
         sem_post(s->sem1);
         sem_wait(s->sem2);
         info(END, 2, s->thread_no);
-
-    }else{
-
-    info(BEGIN, 2, s->thread_no);
-    info(END, 2, s->thread_no);
     }
+    else
+    {
+
+        info(BEGIN, 2, s->thread_no);
+        info(END, 2, s->thread_no);
+    }
+
+    return NULL;
+}
+
+void *threadFn5(void *param)
+{
+    TH_STRUCT *s = (TH_STRUCT *)param;
+
+    
+
+        info(BEGIN, 5, s->thread_no);
+        info(END, 5, s->thread_no);
     
 
     return NULL;
@@ -45,27 +60,32 @@ int main()
     init();
     TH_STRUCT params[5];
     pthread_t tid[5];
-    
+
+    TH_STRUCT params5[45];
+    pthread_t tid5[45];
+
     info(BEGIN, 1, 0);
 
     int pid = fork();
     if (pid == 0)
     {
         info(BEGIN, 2, 0);
-    
+
         sem_t sem1, sem2;
-        sem_init(&sem1,0,0);
-        sem_init(&sem2,0,0);
-    
-        for(int i = 0; i < 5; i++){
-            params[i].thread_no = i+1;
+        sem_init(&sem1, 0, 0);
+        sem_init(&sem2, 0, 0);
+
+        for (int i = 0; i < 5; i++)
+        {
+            params[i].thread_no = i + 1;
             params[i].sem1 = &sem1;
             params[i].sem2 = &sem2;
-             
+
             pthread_create(&tid[i], NULL, threadFn, &params[i]);
         }
 
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++)
+        {
             pthread_join(tid[i], NULL);
         }
 
@@ -93,6 +113,24 @@ int main()
         if (pid5 == 0)
         {
             info(BEGIN, 5, 0);
+
+            for (int i = 0; i < 45; i++)
+            {
+                params5[i].thread_no = i + 1;
+                params5[i].sem1 = &sem1;
+                params5[i].sem2 = &sem2;
+
+                pthread_create(&tid5[i], NULL, threadFn5, &params5[i]);
+            }
+
+            for (int i = 0; i < 45; i++)
+            {
+                pthread_join(tid5[i], NULL);
+            }
+
+            sem_destroy(&sem1);
+            sem_destroy(&sem2);
+
             int pid7 = fork();
             if (pid7 == 0)
             {
@@ -102,7 +140,6 @@ int main()
                 exit(0);
             }
             waitpid(pid7, NULL, 0);
-
 
             info(END, 5, 0);
             exit(0);
@@ -121,7 +158,6 @@ int main()
         waitpid(pid5, NULL, 0);
         waitpid(pid4, NULL, 0);
         waitpid(pid3, NULL, 0);
-        
 
         info(END, 2, 0);
         exit(0);
